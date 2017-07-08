fc210e304d5e32ddcbb940d2da251132303e98a5
Alexander Bogushov <abogushov@gmail.com>
2017-07-08 18:15:05 +0300

    Bake projects in the tests folder.

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import py
+
+import pytest
+from pytest_cookies import Cookies
+
+
+@pytest.fixture
+def cookies(request, _cookiecutter_config_file):
+    template_dir = request.config.option.template
+    py.path.local('tests/baked').remove()
+    output_factory = py.path.local('tests').mkdir('baked').mkdir
+    return Cookies(template_dir, output_factory, _cookiecutter_config_file)
