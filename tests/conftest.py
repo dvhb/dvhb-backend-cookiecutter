ad01a072dda48bc706befc55f8fe014666f54bed
Alexander Bogushov <abogushov@gmail.com>
2018-01-05 17:04:16 +0500

    🔧 Change folder of baked projects.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,13 @@ import pytest
 from pytest_cookies import Cookies
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def cookies(request, _cookiecutter_config_file):
     template_dir = request.config.option.template
-    p = py.path.local('tests/baked')
+    p = py.path.local('tests/projects')
     if p.exists():
         p.remove()
-    output_factory = py.path.local('tests').mkdir('baked').mkdir
+    output_factory = py.path.local('tests').mkdir('projects').mkdir
     return Cookies(template_dir, output_factory, _cookiecutter_config_file)
 
 
