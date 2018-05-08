f37059deb97f5152e84dc85f7b3a5e1c23c185d9
Alexander Malev <am@dvhb.ru>
2018-05-08 05:33:22 +0300

    app++

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,6 +1,20 @@
+from pathlib import Path
+
 from . import wsgi  # noqa
+from aioworkers.core.config import Config
 
 try:
     from .version import VERSION as __version__
 except ImportError:
     __version__ = 'DEV'
+
+
+BASE = Path(__file__).parent
+
+configs = (
+    BASE / 'config.yaml',
+)
+
+
+def get_config():
+    return Config().load(*configs)
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/web.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/web.py
@@ -2,12 +2,9 @@ import collections
 import os
 
 import django
-import aiopg.sa
-import aioredis
 import aiohttp_jinja2
 import jinja2
-import aioworkers.http
-from aiohttp_apiset.middlewares import jsonify
+import aioworkers_aiohttp.app
 
 from dvhb_hybrid.config import dirs
 from dvhb_hybrid.amodels import AppModels
@@ -16,67 +13,32 @@ from .settings import BASE_DIR
 
 import {{cookiecutter.project_slug}}
 
+from aiohttp_wsgi import WSGIHandler
+
+wsgi_handler = WSGIHandler({{cookiecutter.project_slug}}.wsgi.application)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.project_slug}}.settings')
 django.setup()
 
 AppModels.import_all_models_from_packages({{cookiecutter.project_slug}})
 
 
-class Application(aioworkers.http.Application):
-    def __init__(self, **kwargs):
-        kwargs['debug'] = kwargs['config'].debug
+class Application(aioworkers_aiohttp.app.Application):
+    def __init__(self, config, *args, **kwargs):
         from {{cookiecutter.project_slug}}.router import create_router
         router = create_router()
+        router.add_route("*", "/{path_info:(admin|static|media).*}", wsgi_handler)
 
-        super().__init__(router=router, **kwargs, middlewares=[jsonify])
-
+        super().__init__(config, *args, router=router, **kwargs)
         self['state'] = collections.Counter()
-
-        self.models = self.m = AppModels(self)
 
         aiohttp_jinja2.setup(
             self, loader=jinja2.FileSystemLoader(
-                dirs(self.config.path.templates, base_dir=BASE_DIR)),
+                dirs(self.context.config.path.templates, base_dir=BASE_DIR)),
         )
-
-        cls = type(self)
-        self.on_startup.append(cls.startup_database)
-        self.on_startup.append(cls.startup_redis)
-        self.on_cleanup.append(cls.cleanup_database)
-        self.on_cleanup.append(cls.cleanup_redis)
-
-    async def startup_redis(self):
-        config = self.config.redis.default
-        self['redis'] = await aioredis.create_pool(
-            (config.host, config.port),
-            db=config.db,
-            minsize=config.minsize,
-            maxsize=config.maxsize,
-            loop=self.loop)
-        config = self.config.redis.sessions
-        self['sessions'] = await aioredis.create_pool(
-            (config.host, config.port),
-            db=config.db,
-            minsize=config.minsize,
-            maxsize=config.maxsize,
-            loop=self.loop)
-
-    async def cleanup_redis(self):
-        await self['redis'].clear()
-        await self['sessions'].clear()
-
-    async def startup_database(self):
-        dbparams = self.config.databases.default
-        self['db'] = await aiopg.sa.create_engine(**dbparams)
-        self.models = self.m = AppModels(self)
-
-    async def cleanup_database(self):
-        async with self['db']:
-            pass
 
     @property
     def name(self):
-        return self.config.app.name
+        return self.config.name
 
     @property
     def sessions(self):
